daa9838b50fefaaf2b967a1df82969e7a6e4be06
tslater2006 <tslater2006@gmail.com>
2021-11-30 23:57:08 -0600

    Improved Day 1 after seeing other Rust solutions

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -4,43 +4,27 @@ pub fn solve_part_1() {
     let list_of_ints: Vec<i32> = INPUT.lines().map(|f| f.parse::<i32>().unwrap()).collect();
 
     //println!("Part 1 answer: {:?}", list_of_ints);
-    let increase_count = count_increases(&list_of_ints);
+    let mut increase_count = 0;
+    for x in 1..list_of_ints.len() {
+        if list_of_ints[x] > list_of_ints[x-1] {
+            increase_count += 1;
+        }
+    }
+
     println!("Part 1 Answer: {}", increase_count);
 }
 
-fn count_increases(vec: &Vec<i32>) -> i32 {
-    let mut last_num = vec[0];
-    let mut increase_count = 0;
-    for x in 1..vec.len() {
-        if vec[x] > last_num {
-            increase_count += 1;
-        }
-        last_num = vec[x];
-    }
-
-    increase_count
-}
 
 pub fn solve_part_2() {
     let list_of_ints: Vec<i32> = INPUT.lines().map(|f| f.parse::<i32>().unwrap()).collect();
+    let mut increases = 0;
+    for x in 3..list_of_ints.len() {
+        let a = list_of_ints[x-3] + list_of_ints[x-2] + list_of_ints[x-1];
+        let b = list_of_ints[x] + list_of_ints[x-1] + list_of_ints[x-2];
 
-    let mut sums_of_three: Vec<i32> = Vec::new();
-    let mut current_sum = 0;
-    let mut index = 0;
-    let mut nums_added = 0;
-    while index < list_of_ints.len() {
-        current_sum += list_of_ints[index];
-        nums_added += 1;
-        if nums_added == 3 {
-            sums_of_three.push(current_sum);
-            current_sum = 0;
-            index -= 1;
-            nums_added = 0;
-        } else {
-            index += 1;
+        if b > a {
+            increases += 1;
         }
     }
-
-    let answer = count_increases(&sums_of_three);
-    println!("Part 2 Answer: {}", answer);
+    println!("Part 2 Answer: {}", increases);
 }
