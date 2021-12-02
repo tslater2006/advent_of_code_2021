ea8e4e3ba4faf85bde5469a9cbe38b85401ac6cc
tslater2006 <tslater2006@gmail.com>
2021-12-01 23:51:37 -0600

    Add day number to output

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -5,7 +5,7 @@ pub fn solve_part_1() {
 
     let part1_answer = list_of_ints.windows(2).filter(|f| f[0] < f[1]).count();
 
-    println!("Part 1 Answer: {}", part1_answer);
+    println!("Day #1 Part 1 Answer: {}", part1_answer);
     /* let mut increase_count = 0;
     for x in 1..list_of_ints.len() {
         if list_of_ints[x] > list_of_ints[x-1] {
@@ -22,7 +22,7 @@ pub fn solve_part_2() {
     let groups_of_three: Vec<i32> = list_of_ints.windows(3).map(|w| w.iter().sum()).collect();
 
     let part2_answer = groups_of_three.windows(2).filter(|f| f[0] < f[1]).count();
-    println!("Part 2 Answer: {}", part2_answer);
+    println!("Day #1 Part 2 Answer: {}", part2_answer);
 
     /* let mut increases = 0;
     for x in 3..list_of_ints.len() {
