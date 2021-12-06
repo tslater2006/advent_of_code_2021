9eebe6835273f5fceb0c2ffa8ec02204e037594e
tslater2006 <tslater2006@gmail.com>
2021-12-06 00:44:20 -0600

    Day 6 Solution

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,29 +1,49 @@
+use std::{collections::HashMap};
+
 const INPUT: &str = include_str!("..\\..\\inputs\\day6.txt");
 
-pub fn solve_part_1() {
-    let mut fish: Vec<u8> = INPUT.split(",").map(|s| s.parse().unwrap()).collect();
-
-    for day in 0..80 {
-        let mut new_fish_count = 0;
-        for f in fish.iter_mut() {
-            match *f {
-                0 => {
-                    *f = 6;
-                    new_fish_count += 1;
-                }
-                _ => {
-                    *f -=1;
-                }
+fn get_spawn_count(cache: &mut HashMap<(usize, usize), usize>, age: usize, days: usize) -> usize {
+    match cache.get(&(age, days)) {
+        Some(v) => {
+            return *v;
+        }
+        None => {
+            let mut spawn_count = 0;
+            if days < 7 || days < (age + 1) {
+                return spawn_count;
             }
-        }
+            let num_fish_made: usize = 1 + ((days - (age + 1)) / 7);
 
-        for n in 0..new_fish_count{
-            fish.push(8);
-        }
+            for x in 0..num_fish_made {
+                let fish_start_day = days - (age + 1) - (7 * x);
+                spawn_count += 1 + get_spawn_count(cache, 8, fish_start_day);
+            }
 
+            cache.insert((age,days),spawn_count);
+            spawn_count
+        }
     }
-
-    println!("{}", fish.len());
 }
 
-pub fn solve_part_2() {}
+pub fn solve_part_1() {
+    let fish: Vec<u8> = INPUT.split(",").map(|s| s.parse().unwrap()).collect();
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+    let ans: usize = fish
+        .iter()
+        .map(|f| 1 + get_spawn_count(&mut cache, *f as usize, 80))
+        .sum();
+
+    println!("Answer: {}", ans);
+
+}
+
+pub fn solve_part_2() {
+    let fish: Vec<u8> = INPUT.split(",").map(|s| s.parse().unwrap()).collect();
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+    let ans: usize = fish
+        .iter()
+        .map(|f| 1 + get_spawn_count(&mut cache, *f as usize, 256))
+        .sum();
+
+    println!("Answer: {}", ans);
+}
